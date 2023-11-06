a7918b703cad25c0a917f66359ce1ee816f63da4
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-11-06 14:48:17 +0400

    experts section added

--- src/Components/Experts/Experts.jsx
+++ src/Components/Experts/Experts.jsx
@@ -1,15 +1,20 @@
-
+import {FaBehance, FaFacebook, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 
 const Experts = () => {
     return (
-        <div className="grid grid-cols-3 gap-4 my-7 ">
+        <div >
+            <h2 className='border-b-2 text-3xl font-medium mb-4 w-full'>Meet Our Experts</h2>
+            <div className="grid grid-cols-3 gap-4 my-7 ">
             <div className="card card-compact w-96 bg-yellow-100  shadow-xl">
                 <figure><img src="https://i.ibb.co/gFSxTqy/chef-1.jpg" alt="Chef" /></figure>
                 <div className="card-body text-center">
                     <h2 className="card-title justify-center">Mr. Rauf</h2>
                     <p>Senior Chef</p>
-                    <div className="card-actions justify-end">
-                    
+                    <div className="card-actions justify-center">
+                      <FaFacebook></FaFacebook>
+                      <FaLinkedinIn></FaLinkedinIn>
+                      <FaTwitter></FaTwitter>
+                      <FaBehance></FaBehance>
                     </div>
                 </div>
             </div>
@@ -18,8 +23,11 @@ const Experts = () => {
                 <div className="card-body text-center">
                     <h2 className="card-title justify-center">Mr. Rauf</h2>
                     <p>Senior Chef</p>
-                    <div className="card-actions justify-end">
-                        
+                    <div className="card-actions justify-center">
+                    <FaFacebook></FaFacebook>
+                      <FaLinkedinIn></FaLinkedinIn>
+                      <FaTwitter></FaTwitter>
+                      <FaBehance></FaBehance>
                     </div>
                 </div>
             </div>
@@ -28,10 +36,14 @@ const Experts = () => {
                 <div className="card-body text-center">
                     <h2 className="card-title justify-center">Mr. Rauf</h2>
                     <p>Senior Chef</p>
-                    <div className="card-actions justify-end">
-                        
+                    <div className="card-actions justify-center">
+                    <FaFacebook></FaFacebook>
+                      <FaLinkedinIn></FaLinkedinIn>
+                      <FaTwitter></FaTwitter>
+                      <FaBehance></FaBehance>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     );
